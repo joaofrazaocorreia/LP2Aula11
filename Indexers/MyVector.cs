ab2eaaa6aa786d9d62dadf05d896b3dd85cf2489
joaofrazaocorreia <joaopfrazaocorreia@gmail.com>
2023-12-05 16:49:20 +0000

    Ex 3

---
+++ Indexers/MyVector.cs
@@ -0,0 +1,115 @@
+﻿namespace Indexers
+{
+    public struct MyVector
+    {
+        public float X { get; set; }
+        public float Y { get; set; }
+        public MyVector(float x, float y)
+        {
+            X = x;
+            Y = y;
+        }
+
+        public float this[string s]
+        {
+            get
+            {
+                switch(s.ToLower())
+                {
+                    case "x":
+                        return X;
+
+                    case "a":
+                        return X;
+                        
+                    case "0":
+                        return X;
+
+
+                    case "y":
+                        return Y;
+
+                    case "b":
+                        return Y;
+
+                    case "1":
+                        return Y;
+
+
+                    default:
+                        return 0;
+                }
+            }
+
+            set
+            {
+                switch(s.ToLower())
+                {
+                    case "x":
+                        X = value;
+                        break;
+                    
+                    case "a":
+                        X = value;
+                        break;
+                    
+                    case "0":
+                        X = value;
+                        break;
+
+
+                    case "y":
+                        Y = value;
+                        break;
+
+                    case "b":
+                        Y = value;
+                        break;
+
+                    case "1":
+                        Y = value;
+                        break;
+                        
+
+                    default:
+                        break;
+                }
+            }
+        }
+
+        public float this[int i]
+        {
+            get
+            {
+                switch(i)
+                {
+                    case 0:
+                        return X;
+
+                    case 1:
+                        return Y;
+                        
+                    default:
+                        return 0;
+                }
+            }
+
+            set
+            {
+                switch(i)
+                {
+                    case 0:
+                        X = value;
+                        break;
+
+                    case 1:
+                        Y = value;
+                        break;
+
+                    default:
+                        break;
+                }
+            }
+        }
+    }
+}
---
+++ Indexers/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Indexers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyVector myVector = new MyVector(1.0f, 2.0f);
+
+            Console.WriteLine($"starting vector: ({myVector[0]},{myVector["1"]})");
+
+            myVector[1] = 69.9f;
+            Console.WriteLine($"new Y: {myVector["y"]}");
+
+            myVector["x"] = 42.0f;
+            Console.WriteLine($"new X: {myVector["a"]}");
+        }
+    }
+}
